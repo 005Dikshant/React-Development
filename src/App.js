fc7d4cdde960132d158b7f5bb4fff1d1849c86df
Dikshant Yadav <37838049+005Dikshant@users.noreply.github.com>
2024-03-13 18:42:39 -0400

    Useful
    In this point to be noted, that setCount would cause counter component to be rendered, so dont put any thing inside the setCount(), think of way to manage on top of function, like how date is managed with the count logic, so every re-render it fits are requirment

--- src/App.js
+++ src/App.js
@@ -1,1 +1,39 @@
-export default function App() {}
+import { useState } from "react";
+
+export default function App() {
+  return <Counter />;
+}
+
+function Counter() {
+  const date = new Date();
+  const [count, setCount] = useState(0);
+  date.setDate(date.getDate() + count);
+
+  const [state, setState] = useState(1);
+
+  return (
+    <div>
+      <div>
+        <button onClick={() => setState((s) => s - 1)}>-</button>
+        <span>State: {state}</span>
+        <button onClick={() => setState((s) => s + 1)}>+</button>
+      </div>
+      <div>
+        <button onClick={() => setCount((c) => c - state)}>-</button>
+        <span>Count: {count}</span>
+        <button onClick={() => setCount((c) => c + state)}>+</button>
+      </div>
+
+      <p>
+        <span>
+          {count === 0
+            ? "Today is "
+            : count > 0
+            ? `${count} day from  today is `
+            : `${count} day ago was `}
+        </span>
+        {date.toDateString()}
+      </p>
+    </div>
+  );
+}
