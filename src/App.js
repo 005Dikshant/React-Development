b6e851205969dfb1f97a7f3b57883aee518ebd3e
Dikshant Yadav <37838049+005Dikshant@users.noreply.github.com>
2024-03-19 09:59:41 -0400

    Update App.js
    In this key takeout:
    1. New additional state is created, for the managing the state of items, shown in the UI.
    2. While this new state earlier resides in the form component and that was required to be updated in PackaingList component. So data/state was required to be shared between sibling components.
    3. So in order to resolve it, we lift up the common state items to App component, which is the immediate/closet parent to both Form and PackagingList component and shared the state in the form of props.
    4. In the packagingList component, state is passed as a normal variable and whereas in form component, data is shared as function in the form of props.
    5. Now it is the responsibility of form component to invoke the setter/prop function, to update the state of items data.
    6. Something, that might be confusing be, in the props to form component, it is written like this `onAddItems={handleAddItems} `, but handleAddItems requires parameters, isn't be weird that we dont mention parameters that are required for this function to work. !!! :/

--- src/App.js
+++ src/App.js
@@ -7,11 +7,17 @@ const initialItems = [
 ];
 
 export default function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
+
   return (
     <div className="app">
       <Logo />
-      <Form />
-      <PackagingList />
+      <Form onAddItems={handleAddItems} />
+      <PackagingList items={items} />
       <Stats />
     </div>
   );
@@ -21,7 +27,7 @@ function Logo() {
   return <h1>🏝️ Far Away 🧳</h1>;
 }
 
-function Form() {
+function Form({ onAddItems }) {
   const [description, setDescription] = useState("");
   const [quantity, setQuantity] = useState(1);
 
@@ -38,6 +44,7 @@ function Form() {
       id: Date.now(),
       packed: false,
     };
+    onAddItems(newItem);
 
     console.log(newItem);
     setDescription("");
@@ -67,11 +74,11 @@ function Form() {
   );
 }
 
-function PackagingList() {
+function PackagingList({ items }) {
   return (
     <div className="list">
       <ul>
-        {initialItems.map((item) => (
+        {items.map((item) => (
           <Item item={item} key={item.id} />
         ))}
       </ul>
