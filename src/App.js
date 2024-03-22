30e81bd73f4c9a1dc3bb343aafc8c99c1c6f4f1b
Dikshant Yadav <37838049+005Dikshant@users.noreply.github.com>
2024-03-22 00:59:16 -0400

    Update App.js
    Added sorting functionality in the application.
    
    Key-take aways:
    1. controlled element.
    2. derived state concept on the sortedItems.
    3. Vanilla JS core concept like sort method

--- src/App.js
+++ src/App.js
@@ -83,10 +83,26 @@ function Form({ onAddItems }) {
 }
 
 function PackagingList({ items, onDeleteItem, onUpdateItem }) {
+  const [sortBy, setSortBy] = useState("input");
+
+  let sortedItems;
+
+  if (sortBy === "input") sortedItems = items;
+
+  if (sortBy === "description")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
+
+  if (sortBy === "packed")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => Number(a.packed) - Number(b.packed));
+
   return (
     <div className="list">
       <ul>
-        {items.map((item) => (
+        {sortedItems.map((item) => (
           <Item
             item={item}
             key={item.id}
@@ -95,6 +111,13 @@ function PackagingList({ items, onDeleteItem, onUpdateItem }) {
           />
         ))}
       </ul>
+      <div className="actions">
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="input">sort by input order</option>
+          <option value="description">sort by description</option>
+          <option value="packed">sort by packed stats</option>
+        </select>
+      </div>
     </div>
   );
 }
