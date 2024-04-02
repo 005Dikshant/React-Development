79b41fc698eb2b58a7498bf46be0c42ddb964c56
Dikshant Yadav <37838049+005Dikshant@users.noreply.github.com>
2024-04-01 22:42:50 -0400

    Update App.js
    key-points
    1. Using controlled elements for form handling and adding some logics to make sure the values are correct and as per requirements

--- src/App.js
+++ src/App.js
@@ -156,23 +156,45 @@ function FormAddFriend({ onAddFriend }) {
 }
 
 function FormSplitBill({ selectedFriend }) {
+  const [bill, setBill] = useState("");
+  const [paidByUser, setPaidByUser] = useState("");
+  const paidByFriend = bill ? (paidByUser ? bill - paidByUser : "") : "";
+  const [whoIsPaying, setWhoIsPaying] = useState("user");
+
   return (
     <form className="form-split-bill">
       <h2>Split a bill with {selectedFriend.name}</h2>
 
       <label>💰 Bill value</label>
-      <input type="text" />
+      <input
+        type="text"
+        value={bill}
+        onChange={(e) => setBill(Number(e.target.value))}
+      />
 
       <label>👯‍♂️ Your expense</label>
-      <input type="text" />
+      <input
+        type="text"
+        value={paidByUser}
+        onChange={(e) =>
+          setPaidByUser(
+            Number(e.target.value) >= Number(bill)
+              ? paidByUser
+              : Number(e.target.value)
+          )
+        }
+      />
 
       <label>😎 {selectedFriend.name}'s expense:</label>
-      <input type="text" disabled />
+      <input type="text" disabled value={paidByFriend} />
 
       <label>🤑 who is paying the bill</label>
-      <select>
-        <option value="you">You</option>
-        <option vaue="friend">{selectedFriend.name}</option>
+      <select
+        value={whoIsPaying}
+        onChange={(e) => setWhoIsPaying(e.target.value)}
+      >
+        <option value="user">You</option>
+        <option value="friend">{selectedFriend.name}</option>
       </select>
     </form>
   );
