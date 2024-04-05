0eeb2766cbfebb304219a51df6aa78317435b4a5
Dikshant Yadav <37838049+005Dikshant@users.noreply.github.com>
2024-04-05 12:17:40 -0400

    Update App.js
    Another of doing component-composition without children prop, by adding an external prop and sending whole JSX into that prop

--- src/App.js
+++ src/App.js
@@ -58,7 +58,16 @@ export default function App() {
       </NavBar>
 
       <Main>
-        <Box>
+        <Box element={<MovieList movies={movies} />} />
+        <Box
+          element={
+            <>
+              <WatchedSummary watched={watched} />
+              <WatchedMoviesList watched={watched} />
+            </>
+          }
+        />
+        {/* <Box>
           <MovieList movies={movies} />
         </Box>
 
@@ -67,7 +76,7 @@ export default function App() {
             <WatchedSummary watched={watched} />
             <WatchedMoviesList watched={watched} />
           </>
-        </Box>
+        </Box> */}
       </Main>
     </>
   );
@@ -116,14 +125,14 @@ function Main({ children }) {
   return <main className="main">{children}</main>;
 }
 
-function Box({ children }) {
+function Box({ element }) {
   const [isOpen, setIsOpen] = useState(true);
   return (
     <div className="box">
       <button className="btn-toggle" onClick={() => setIsOpen((open) => !open)}>
         {isOpen ? "–" : "+"}
       </button>
-      {isOpen && children}
+      {isOpen && element}
     </div>
   );
 }
