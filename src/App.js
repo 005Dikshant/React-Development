7ced9ba2503af2383c2340d6c761c0a37c7346dd
Dikshant Yadav <37838049+005Dikshant@users.noreply.github.com>
2024-04-05 11:05:29 -0400

    Update App.js
    Using Component-Composition - via children prop we are eliminating the problem of prop drilling. Since we are calling the required child component to where the state actually lives

--- src/App.js
+++ src/App.js
@@ -51,18 +51,20 @@ export default function App() {
   const [movies, setMovies] = useState(tempMovieData);
   return (
     <>
-      <NavBar movies={movies} />
+      <NavBar>
+        <Search />
+        <NumResults movies={movies} />
+      </NavBar>
       <Main movies={movies} />
     </>
   );
 }
 
-function NavBar({ movies }) {
+function NavBar({ children }) {
   return (
     <nav className="nav-bar">
       <Logo />
-      <Search />
-      <NumResults movies={movies} />
+      {children}
     </nav>
   );
 }
