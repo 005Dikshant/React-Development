5cbc22babfda0e337afe578c3bbbd29de6854611
Dikshant Yadav <37838049+005Dikshant@users.noreply.github.com>
2024-05-04 23:08:32 -0400

    Update StarRating.js
    Setting the component input parameters type, so just to make sure consumer is consuming in aspected format.
    
    So PropTypes can be react and they are already installed when react-project is created.

--- src/StarRating.js
+++ src/StarRating.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import PropTypes from "prop-types";
 
 const containerStyle = {
   display: "flex",
@@ -8,6 +9,15 @@ const containerStyle = {
 const starContainerStyle = {
   display: "flex",
   gap: "4px",
+};
+
+StarRating.propTypes = {
+  maxRating: PropTypes.number,
+  color: PropTypes.string,
+  size: PropTypes.number,
+  messages: PropTypes.array,
+  defaultRating: PropTypes.number,
+  onSetRating: PropTypes.func,
 };
 
 export default function StarRating({
@@ -24,6 +34,7 @@ export default function StarRating({
     color,
     fontSize: `${size / 1.5}px`,
   };
+
   const [rating, setRating] = useState(defaultRating);
   const [tempRating, setTempRating] = useState(0);
 
