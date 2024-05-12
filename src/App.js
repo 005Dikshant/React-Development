738e2bb4a2a770e1d3714fc7bc2045dda4c11bcb
Dikshant Yadav <37838049+005Dikshant@users.noreply.github.com>
2024-05-11 22:30:23 -0400

    Update App.js
    Showing loading component when the component is fetching the data or so doing some process, just to keep UI in check with users.

--- src/App.js
+++ src/App.js
@@ -52,14 +52,17 @@ const query = "american pie";
 export default function App() {
   const [movies, setMovies] = useState([]);
   const [watched, setWatched] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     async function fetchMovies() {
+      setIsLoading(true);
       const res = await fetch(
         `http://www.omdbapi.com/?apikey=${key}&s=${query}`
       );
       const data = await res.json();
       setMovies(data.Search);
+      setIsLoading(false);
       console.log(data.Search);
     }
 
@@ -74,9 +77,7 @@ export default function App() {
       </NavBar>
 
       <Main>
-        <Box>
-          <MovieList movies={movies} />
-        </Box>
+        <Box>{isLoading ? <Loader /> : <MovieList movies={movies} />}</Box>
 
         <Box>
           <WatchedSummary watched={watched} />
@@ -85,6 +86,10 @@ export default function App() {
       </Main>
     </>
   );
+}
+
+function Loader() {
+  return <p className="loader">Loading...</p>;
 }
 
 function NavBar({ children }) {
