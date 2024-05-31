a0c7a839409c83d72a29bdaa67b9187258515ed9
Dikshant Yadav <37838049+005Dikshant@users.noreply.github.com>
2024-05-31 16:24:17 -0400

    Moving to next questions
    Keytake-away:
    
    1. Creating a new action on reducer called 'nextQuestion' to get the next question.
    
    2. We set ans == null on that as well, because the new question don't require the previous answer data to be used.

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ import Loader from "./Loader";
 import Error from "./Error";
 import StartScreen from "./StartScreen";
 import Question from "./Question";
+import NextQuestion from "./NextQuestion";
 
 const initialState = {
   questions: [],
@@ -32,6 +33,8 @@ function reducer(state, action) {
             ? state.points + question.points
             : state.points,
       };
+    case "nextQuestion":
+      return { ...state, index: state.index + 1, answer: null };
     default:
       throw new Error("action unknown");
   }
@@ -65,11 +68,14 @@ export default function App() {
           <StartScreen numOfQuestions={numOfQuestions} dispatch={dispatch} />
         )}
         {status === "active" && (
-          <Question
-            question={questions.at(index)}
-            dispatch={dispatch}
-            answer={answer}
-          />
+          <>
+            <Question
+              question={questions.at(index)}
+              dispatch={dispatch}
+              answer={answer}
+            />
+            <NextQuestion dispatch={dispatch} answer={answer} />
+          </>
         )}
       </Main>
     </div>
