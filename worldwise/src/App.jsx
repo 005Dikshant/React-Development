6c7d7eccdb92f7c69c37deed8f201570f97f0336
Dikshant Yadav <37838049+005Dikshant@users.noreply.github.com>
2024-06-09 17:51:16 -0400

    Setting up web pages
    Keytake aways:
    
    1.  Setting up the web pages like adding Link to home page
    
    2. Adding the Pagnav to homepage ,Pricing,Product and login
    
    3. Adding the logo component to pageNav component
    
    4. Adding login route to App.jsx and handling the unknown routes.

--- worldwise/src/App.jsx
+++ worldwise/src/App.jsx
@@ -4,6 +4,8 @@ import Homepage from "./pages/Homepage";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import AppLayout from "./pages/AppLayout";
+import Login from "./pages/Login";
+import PageNotFound from "./pages/PageNotFound";
 
 export default function App() {
   return (
@@ -13,6 +15,8 @@ export default function App() {
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
         <Route path="app" element={<AppLayout />} />
+        <Route path="login" element={<Login />} />
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
   );
--- worldwise/src/pages/PageNotFound.jsx
+++ worldwise/src/pages/PageNotFound.jsx
@@ -1,11 +1,7 @@
-import React from "react";
-import PageNav from "../components/PageNav";
-
-export default function Pagenotfound() {
+export default function PageNotFound() {
   return (
     <div>
-      <PageNav />
-      <h1>Pagenotfound</h1>
+      <h1>Page not found 😢</h1>
     </div>
   );
 }
