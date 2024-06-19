f02a4b14d2f7cbc9151df12e98689979683983d4
Dikshant Yadav <37838049+005Dikshant@users.noreply.github.com>
2024-06-19 15:35:30 -0400

    Adding markers to the map for the given cities
    keytake aways:
    
    1. adding markers for the given cities
    
    2. Difference b/w cities.map(cties => {<h1>hello <h1>}) , cities.map(cities => <h1> hello </h1>)  , cities.map(cties => { return <h1>hello <h1>})

--- worldwise/src/components/Map.jsx
+++ worldwise/src/components/Map.jsx
@@ -1,8 +1,9 @@
 import { useNavigate, useSearchParams } from "react-router-dom";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { useState } from "react";
 
 import styles from "./Map.module.css";
-import { useState } from "react";
+import { useCities } from "../contexts/CityContext";
 
 function Map() {
   const [searchParams, setSearchParams] = useSearchParams();
@@ -12,6 +13,7 @@ function Map() {
   const [mapPosition, setMapPosition] = useState([40, 0]);
 
   const navigate = useNavigate();
+  const { cities } = useCities();
 
   return (
     <div className={styles.mapContainer}>
@@ -25,11 +27,17 @@ function Map() {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
         />
-        <Marker position={mapPosition}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
+        {cities.map((city) => (
+          <Marker
+            position={[city.position.lat, city.position.lng]}
+            key={city.id}
+          >
+            <Popup>
+              {city.cityName} {city.emoji}
+            </Popup>
+          </Marker>
+        ))}
+        ;
       </MapContainer>
     </div>
   );
