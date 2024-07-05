09fb8346ace22d51845cb007418dc6a04cfd991d
Dikshant Yadav <37838049+005Dikshant@users.noreply.github.com>
2024-07-05 02:29:18 -0400

    UserAuthentication && URL handling
    1. We created separate context for user authentication, and then used the same recipe to use the context easily.
    
    2. Rendered User component into AppLayout component.
    
    3. Handled the Login component, and always use onSubmit when there is form dont use button onClick event and always e.preventDefault() to prevent the page from doing hard-reload or refresh.

--- worldwise/src/App.jsx
+++ worldwise/src/App.jsx
@@ -11,26 +11,29 @@ import CountryList from "./components/CountryList";
 import City from "./components/City";
 import Form from "./components/Form";
 import { CityProvider } from "./contexts/CityContext";
+import { AuthProvider } from "./contexts/FakeAuthContext";
 
 export default function App() {
   return (
     <CityProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route index element={<Homepage />} />
-          <Route path="product" element={<Product />} />
-          <Route path="pricing" element={<Pricing />} />
-          <Route path="app" element={<AppLayout />}>
-            <Route path="cities/:id" element={<City />} />
-            <Route index element={<Navigate to="cities" replace />} />
-            <Route path="cities" element={<CityList />} />
-            <Route path="countries" element={<CountryList />} />
-            <Route path="form" element={<Form />} />
-          </Route>
-          <Route path="login" element={<Login />} />
-          <Route path="*" element={<PageNotFound />} />
-        </Routes>
-      </BrowserRouter>
+      <AuthProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route index element={<Homepage />} />
+            <Route path="product" element={<Product />} />
+            <Route path="pricing" element={<Pricing />} />
+            <Route path="app" element={<AppLayout />}>
+              <Route path="cities/:id" element={<City />} />
+              <Route index element={<Navigate to="cities" replace />} />
+              <Route path="cities" element={<CityList />} />
+              <Route path="countries" element={<CountryList />} />
+              <Route path="form" element={<Form />} />
+            </Route>
+            <Route path="login" element={<Login />} />
+            <Route path="*" element={<PageNotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
     </CityProvider>
   );
 }
--- worldwise/src/pages/AppLayout.jsx
+++ worldwise/src/pages/AppLayout.jsx
@@ -1,10 +1,12 @@
 import Siderbar from "../components/Siderbar";
 import Map from "../components/Map";
 import styles from "./AppLayout.module.css";
+import User from "../components/User";
 
 export default function AppLayout() {
   return (
     <div className={styles.app}>
+      <User />
       <Siderbar />
       <Map />
     </div>
