d341d72e58a66dd572603bf87c7ad71dbca216e3
Dikshant Yadav <37838049+005Dikshant@users.noreply.github.com>
2024-07-10 23:54:41 -0400

    Memo Failure
    Keytake aways:
    
    1. when we pass object as a prop to the child component, in which memo fails since the passed props changes over every render.
    
    2. Always remember that function and objects are always created new on every render so hence when parent renders the object created
     is also created newly which is why Archive component also renders on every key-stroke in search bar.

--- src/App-memo.js
+++ src/App-memo.js
@@ -33,6 +33,11 @@ function App() {
     setPosts([]);
   }
 
+  const archiveState = {
+    show: false,
+    title: "Post archive in addition to main Posts",
+  };
+
   // Whenever `isFakeDark` changes, we toggle the `fake-dark-mode` class on the HTML element (see in "Elements" dev tool).
   useEffect(
     function () {
@@ -57,7 +62,7 @@ function App() {
         setSearchQuery={setSearchQuery}
       />
       <Main posts={searchedPosts} onAddPost={handleAddPost} />
-      <Archive show={false} />
+      <Archive archiveState={archiveState} />
       <Footer />
     </section>
   );
@@ -154,18 +159,18 @@ function List({ posts }) {
   );
 }
 
-const Archive = memo(function Archive({ show }) {
+const Archive = memo(function Archive({ archiveState }) {
   // Here we don't need the setter function. We're only using state to store these posts because the callback function passed into useState (which generates the posts) is only called once, on the initial render. So we use this trick as an optimization technique, because if we just used a regular variable, these posts would be re-created on every render. We could also move the posts outside the components, but I wanted to show you this trick 😉
   const [posts] = useState(() =>
     // 💥 WARNING: This might make your computer slow! Try a smaller `length` first
     Array.from({ length: 30000 }, () => createRandomPost())
   );
 
-  const [showArchive, setShowArchive] = useState(show);
+  const [showArchive, setShowArchive] = useState(archiveState.show);
 
   return (
     <aside>
-      <h2>Post archive</h2>
+      <h2>{archiveState.title}</h2>
       <button onClick={() => setShowArchive((s) => !s)}>
         {showArchive ? "Hide archive posts" : "Show archive posts"}
       </button>
