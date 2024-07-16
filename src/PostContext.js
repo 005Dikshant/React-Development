bcdf711f2d9bf98de3e0ef773dffec4e6f8b80b7
Dikshant Yadav <37838049+005Dikshant@users.noreply.github.com>
2024-07-15 20:26:13 -0400

    useMemo with context API
    keytake aways:
    
    1. Memoize the component from stop being rendered from the props change
    
    2. useMemo hook in the context itself.

--- src/PostContext.js
+++ src/PostContext.js
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useMemo, useState } from "react";
 import { faker } from "@faker-js/faker";
 
 function createRandomPost() {
@@ -36,13 +36,15 @@ function PostProvider({ children }) {
     setPosts([]);
   }
 
-  const val = {
-    posts: searchedPosts,
-    onClearPosts: handleClearPosts,
-    searchQuery,
-    setSearchQuery,
-    onAddPost: handleAddPost,
-  };
+  const val = useMemo(() => {
+    return {
+      posts: searchedPosts,
+      onClearPosts: handleClearPosts,
+      searchQuery,
+      setSearchQuery,
+      onAddPost: handleAddPost,
+    };
+  }, [searchQuery, searchedPosts]);
 
   // second step of setting up the values
   return (
