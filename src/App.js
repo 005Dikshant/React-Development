ca546407421f41ca7d32067c1c5523651015a4e5
Dikshant Yadav <37838049+005Dikshant@users.noreply.github.com>
2024-07-24 23:29:44 -0400

    Starter files with some updates

--- src/App.js
+++ src/App.js
@@ -1,1 +1,46 @@
-export default function App() {}
+import { useEffect, useState } from "react";
+import Calculator from "./Calculator";
+import ToggleSounds from "./ToggleSounds";
+import Time from "./Time";
+
+function App() {
+  const [allowSound, setAllowSound] = useState(true);
+
+  // Will be be AM or PM
+  const partOfDay = time.slice(-2);
+
+  const workouts = [
+    {
+      name: "Full-body workout",
+      numExercises: partOfDay === "AM" ? 9 : 8,
+    },
+    {
+      name: "Arms + Legs",
+      numExercises: 6,
+    },
+    {
+      name: "Arms only",
+      numExercises: 3,
+    },
+    {
+      name: "Legs only",
+      numExercises: 4,
+    },
+    {
+      name: "Core only",
+      numExercises: partOfDay === "AM" ? 5 : 4,
+    },
+  ];
+
+  return (
+    <main>
+      <h1>Workout timer</h1>
+      <Time />
+
+      <ToggleSounds allowSound={allowSound} setAllowSound={setAllowSound} />
+      <Calculator workouts={workouts} allowSound={allowSound} />
+    </main>
+  );
+}
+
+export default App;
