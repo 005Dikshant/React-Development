9a6f85e3b71bebd750a098bf4d91a738c773b466
Dikshant Yadav <37838049+005Dikshant@users.noreply.github.com>
2024-07-31 00:08:40 -0400

    useEffect creating complexity
    keytake aways:
    
    1. in this whenever, the user click the + or - button, it causes the set the duration and playing the sound and the component re-renders and which in turns creates new playSound function and because of that the useEffect runs and causes the setDuration to be set again and then playing the sound twice.

--- src/Calculator.js
+++ src/Calculator.js
@@ -8,25 +8,28 @@ function Calculator({ workouts, allowSound }) {
   const [durationBreak, setDurationBreak] = useState(5);
   const [duration, setDuration] = useState(0);
 
-  useEffect(() => {
-    setDuration((number * sets * speed) / 60 + (sets - 1) * durationBreak);
-  }, [number, sets, speed, durationBreak]);
-
-  // const duration = (number * sets * speed) / 60 + (sets - 1) * durationBreak;
-  const mins = Math.floor(duration);
-  const seconds = (duration - mins) * 60;
-
   const playSound = function () {
     if (!allowSound) return;
     const sound = new Audio(clickSound);
     sound.play();
   };
 
+  useEffect(() => {
+    setDuration((number * sets * speed) / 60 + (sets - 1) * durationBreak);
+    playSound();
+  }, [number, sets, speed, durationBreak, playSound]);
+
+  // const duration = (number * sets * speed) / 60 + (sets - 1) * durationBreak;
+  const mins = Math.floor(duration);
+  const seconds = (duration - mins) * 60;
+
   function handleInc() {
     setDuration((duration) => Math.floor(duration) + 1);
+    playSound();
   }
   function handleDec() {
     setDuration((duration) => (duration > 1 ? Math.ceil(duration) - 1 : 0));
+    playSound();
   }
 
   return (
