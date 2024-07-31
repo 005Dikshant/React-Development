370a4fec6720fb7ab00fe153f236a2f5aaf9feaf
Dikshant Yadav <37838049+005Dikshant@users.noreply.github.com>
2024-07-31 00:13:19 -0400

    Trying the fix the issue from useEffect
    Keytake aways:
    
    1. So here I thought to move the function out of the component so that it isn't created on every renders newly.
    
    2. Eventually it works, button and sliders works as expected without any hassle
    
    3. But issue is that now allowedSound is passed as dependency inside the react dependency list for useEffect, so when ever the user mutes or unmutes the mic, the effect runs and which in turns set the value for duration to the default value.

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,6 +1,12 @@
 import { memo, useEffect, useState } from "react";
 import clickSound from "./ClickSound.m4a";
 
+const playSound = function (allowSound) {
+  if (!allowSound) return;
+  const sound = new Audio(clickSound);
+  sound.play();
+};
+
 function Calculator({ workouts, allowSound }) {
   const [number, setNumber] = useState(workouts.at(0).numExercises);
   const [sets, setSets] = useState(3);
@@ -8,16 +14,10 @@ function Calculator({ workouts, allowSound }) {
   const [durationBreak, setDurationBreak] = useState(5);
   const [duration, setDuration] = useState(0);
 
-  const playSound = function () {
-    if (!allowSound) return;
-    const sound = new Audio(clickSound);
-    sound.play();
-  };
-
   useEffect(() => {
     setDuration((number * sets * speed) / 60 + (sets - 1) * durationBreak);
-    playSound();
-  }, [number, sets, speed, durationBreak, playSound]);
+    playSound(allowSound);
+  }, [number, sets, speed, durationBreak, allowSound]);
 
   // const duration = (number * sets * speed) / 60 + (sets - 1) * durationBreak;
   const mins = Math.floor(duration);
@@ -25,11 +25,11 @@ function Calculator({ workouts, allowSound }) {
 
   function handleInc() {
     setDuration((duration) => Math.floor(duration) + 1);
-    playSound();
+    playSound(allowSound);
   }
   function handleDec() {
     setDuration((duration) => (duration > 1 ? Math.ceil(duration) - 1 : 0));
-    playSound();
+    playSound(allowSound);
   }
 
   return (
