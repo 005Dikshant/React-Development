82fd7a2a5cbbe53300af45a79d2acb3782a619c0
Dikshant Yadav <37838049+005Dikshant@users.noreply.github.com>
2024-08-04 04:07:25 -0400

    Creating a redux store
    key take aways:
    
    1. Just created a redux store for now, doing the same useReducer hook dispatch function do.
    
    2. Just a bit of redux methods like createStore and store.dispatch and store.getState() methods to dispatch a action, creating a store and reading the state of store.

--- src/store.js
+++ src/store.js
@@ -1,10 +1,12 @@
+import { createStore } from "redux";
+
 const initalState = {
   balance: 0,
   loan: 0,
-  loanPurpose: 0,
+  loanPurpose: "",
 };
 
-function reducer(state, action) {
+function reducer(state = initalState, action) {
   switch (action.type) {
     case "account/deposit":
       return { ...state, balance: state.balance + action.payload };
@@ -12,13 +14,18 @@ function reducer(state, action) {
     case "account/withdraw":
       return { ...state, balance: state.balance - action.payload };
 
-    case "account/loan":
+    case "account/requestLoan":
       if (state.loan > 0) {
         return state;
       }
-      return { ...state, loan: action.payload };
+      return {
+        ...state,
+        loan: action.payload.loan,
+        loanPurpose: action.payload.loanPurpose,
+        balance: state.balance + action.payload.loan,
+      };
 
-    case "accout/payloan":
+    case "account/payLoan":
       return {
         ...state,
         loan: 0,
@@ -30,3 +37,22 @@ function reducer(state, action) {
       return state;
   }
 }
+
+const store = createStore(reducer);
+store.dispatch({ type: "account/deposit", payload: 500 });
+console.log(store.getState());
+
+store.dispatch({ type: "account/withdraw", payload: 200 });
+console.log(store.getState());
+
+store.dispatch({
+  type: "account/requestLoan",
+  payload: { loan: 1000, loanPurpose: "To Buy a car" },
+});
+console.log(store.getState());
+
+store.dispatch({
+  type: "account/payLoan",
+});
+
+console.log(store.getState());
