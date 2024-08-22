4713bfe3387d7e95c8578e0f628afb07016d205a
Dikshant Yadav <37838049+005Dikshant@users.noreply.github.com>
2024-08-22 00:21:08 -0400

    customer Slice with RTK
    Keytake aways:
    
    1. Same thing as the last commit, now just creating the customer slice.
    
    2. And secondly for the deposit method in AccountSlice we are still using the old code because we are doing a side effect which is easier and less complex using the our way of the deposit.

--- src/features/customers/customerSlice.js
+++ src/features/customers/customerSlice.js
@@ -1,41 +1,73 @@
-const initalStateCustomer = {
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
   fullName: "",
   createdAt: "",
   nationalID: "",
 };
 
-export default function customerReducer(state = initalStateCustomer, action) {
-  switch (action.type) {
-    case "customer/createCustomer":
-      return {
-        ...state,
-        fullName: action.payload.fullName,
-        createdAt: action.payload.createdAt,
-        nationalID: action.payload.nationalID,
-      };
-
-    case "customer/updateName":
-      return {
-        ...state,
-        fullName: action.payload,
-      };
-
-    default:
-      return state;
-  }
-}
-
-export function createCustomer(fullName, nationalID) {
-  return {
-    type: "customer/createCustomer",
-    payload: {
-      fullName,
-      nationalID,
-      createdAt: new Date().toISOString(),
+const customerSlice = createSlice({
+  name: "customer",
+  initialState,
+  reducers: {
+    createCustomer: {
+      prepare(fullName, nationalID) {
+        return {
+          payload: {
+            fullName,
+            createdAt: new Date().toISOString(),
+            nationalID,
+          },
+        };
+      },
+      reducer(state, action) {
+        state.fullName = action.payload.fullName;
+        state.createdAt = action.payload.createdAt;
+        state.nationalID = action.payload.nationalID;
+      },
     },
-  };
-}
 
-export function updateName(fullName) {
-  return { type: "customer/updateName", payload: fullName };
-}
+    updateName(state, action) {
+      state.fullName = action.payload;
+    },
+  },
+});
+
+export default customerSlice.reducer;
+export const { createCustomer, updateName } = customerSlice.actions;
+
+// export default function customerReducer(state = initalStateCustomer, action) {
+//   switch (action.type) {
+//     case "customer/createCustomer":
+//       return {
+//         ...state,
+//         fullName: action.payload.fullName,
+//         createdAt: action.payload.createdAt,
+//         nationalID: action.payload.nationalID,
+//       };
+
+//     case "customer/updateName":
+//       return {
+//         ...state,
+//         fullName: action.payload,
+//       };
+
+//     default:
+//       return state;
+//   }
+// }
+
+// export function createCustomer(fullName, nationalID) {
+//   return {
+//     type: "customer/createCustomer",
+//     payload: {
+//       fullName,
+//       nationalID,
+//       createdAt: new Date().toISOString(),
+//     },
+//   };
+// }
+
+// export function updateName(fullName) {
+//   return { type: "customer/updateName", payload: fullName };
+// }
