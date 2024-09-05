89ea43835f19a58f8e7d0ecb8999160796fa2c24
Dikshant Yadav <37838049+005Dikshant@users.noreply.github.com>
2024-09-05 16:20:50 -0400

    Authentication of fields in React Router Dom
    Keytake aways:
    1. const formErrors = useActionData();
    
       We use this hook to get the data from the action handler that return, so generally this kind of approach is used for field/ form authentication
    
    2.  Before hitting the api we check the field that are present inside the form to make sure everything is as per requirement.
    
    const errors = {};
      if (!isValidPhone(order.phone))
        errors.phone =
          "Please give us correct phone number. We might need to contact you.";
    
      if (Object.keys(errors).length > 0) return errors;

--- Pizza Project Vite/src/features/order/CreateOrder.jsx
+++ Pizza Project Vite/src/features/order/CreateOrder.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Form, redirect } from "react-router-dom";
+import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
 import { createOrder } from "../services/apiRestaurant";
 
 // https://uibakery.io/regex-library/phone-number
@@ -36,6 +36,12 @@ function CreateOrder() {
   // const [withPriority, setWithPriority] = useState(false);
   const cart = fakeCart;
 
+  const navigation = useNavigation();
+
+  const formErrors = useActionData();
+
+  const isSubmitting = navigation.state === "submitting";
+
   return (
     <div>
       <h2>Ready to order? Let's go!</h2>
@@ -50,6 +56,7 @@ function CreateOrder() {
           <label>Phone number</label>
           <div>
             <input type="tel" name="phone" required />
+            {formErrors?.phone && <p>{formErrors.phone} </p>}
           </div>
         </div>
 
@@ -72,7 +79,9 @@ function CreateOrder() {
         </div>
 
         <div>
-          <button>Order now</button>
+          <button disabled={isSubmitting}>
+            {isSubmitting ? "Placing an order..." : "Order now"}
+          </button>
         </div>
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
@@ -91,6 +100,13 @@ export async function action({ request }) {
     priority: data.priority === "on" ? true : false,
     cart: JSON.parse(data.cart),
   };
+
+  const errors = {};
+  if (!isValidPhone(order.phone))
+    errors.phone =
+      "Please give us correct phone number. We might need to contact you.";
+
+  if (Object.keys(errors).length > 0) return errors;
 
   const newOrder = await createOrder(order);
 
