ffa7c2ef8f907263e657ae4aee3205b3ee9e2688
Dikshant Yadav <37838049+005Dikshant@users.noreply.github.com>
2024-09-06 08:04:49 -0400

    Tailwind Styling the text
    Keytake aways:
    
    1. If you want to use bigger size than we can use escape hatch like tracking-[val px/em], text-[val px/em]

--- Pizza Project Vite/src/features/ui/Header.jsx
+++ Pizza Project Vite/src/features/ui/Header.jsx
@@ -1,12 +1,15 @@
 import { Link } from "react-router-dom";
 import SearchOrder from "../order/SearchOrder";
+import Username from "../user/Username";
 
 function Header() {
   return (
-    <div className="bg-yellow-500">
-      <Link to="/">FAST REACT PIZZA CO.</Link>
+    <div className="bg-yellow-500 uppercase">
+      <Link to="/" className="tracking-widest">
+        Fast React Pizza Co.
+      </Link>
       <SearchOrder />
-      <p>Dikshant</p>
+      <Username />
     </div>
   );
 }
