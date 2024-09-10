b9bbe39747fd05109e6f8a1c2819106f538d65f8
Dikshant Yadav <37838049+005Dikshant@users.noreply.github.com>
2024-09-10 01:42:39 -0400

    Flex Box Tailwind css
    keytake aways:
    
    1. Media point query in tailwind
    2. flex box is mostly with item-center and justifiy-between tailwind to have items placed on far end of corners.

--- Pizza Project Vite/src/features/ui/Home.jsx
+++ Pizza Project Vite/src/features/ui/Home.jsx
@@ -1,9 +1,9 @@
-import CreateUser from "../user/CreateUser";
+import CreateUser from '../user/CreateUser';
 
 function Home() {
   return (
-    <div className="my-10 text-center sm:my-16">
-      <h1 className="font-semibol mb-8 text-xl">
+    <div className="my-10 px-4 text-center sm:my-16">
+      <h1 className="font-semibol mb-8 text-xl md:text-3xl">
         The best pizza.
         <br />
         <span className="text-yellow-500">
