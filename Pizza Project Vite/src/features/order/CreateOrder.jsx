91ae9b27a9e3813e18541cc0f13b88a908d224cd
Dikshant Yadav <37838049+005Dikshant@users.noreply.github.com>
2024-10-30 00:33:36 -0400

    Updating the state and reading the username state
    Keytake aways:
    
    The function inside the reducers are generally called action creator, since they are one that dispatches the action. and we use useDispatch hook for that.

--- Pizza Project Vite/src/features/order/CreateOrder.jsx
+++ Pizza Project Vite/src/features/order/CreateOrder.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
 import { createOrder } from "../services/apiRestaurant";
 import Button from "../ui/Button";
+import { useSelector } from "react-redux";
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -41,6 +42,8 @@ function CreateOrder() {
 
   const formErrors = useActionData();
 
+  const username = useSelector((state) => state.user.username);
+
   const isSubmitting = navigation.state === "submitting";
 
   return (
@@ -50,7 +53,13 @@ function CreateOrder() {
       <Form method="POST">
         <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
           <label className="sm:basis-40">First Name</label>
-          <input type="text" name="customer" className="input grow" required />
+          <input
+            type="text"
+            name="customer"
+            className="input grow"
+            defaultValue={username}
+            required
+          />
         </div>
 
         <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
--- Pizza Project Vite/src/features/ui/Button.jsx
+++ Pizza Project Vite/src/features/ui/Button.jsx
@@ -13,7 +13,7 @@ function Button({ children, disabled, to, type }) {
 
   if (to) {
     return (
-      <Link className={styles[type]} to="/order/new">
+      <Link className={styles[type]} to={to}>
         {children}
       </Link>
     );
