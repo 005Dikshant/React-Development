992cb71cace3ee6fceb7376b1bde7b0ab9943608
Dikshant Yadav <37838049+005Dikshant@users.noreply.github.com>
2024-11-01 12:26:38 -0400

    Cart Slice and add pizza to cart
    Keytake aways:
    
    1. created the cartSlice to maintain the state across the app. And added 4 action creators and added the reduce to the store.
    
    2. used dispatch to fire the action creator.

---
+++ Pizza Project Vite/src/features/cart/cartSlice.js
@@ -0,0 +1,48 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  cart: [
+    {
+      name: "magritta",
+      pizzaId: 12,
+      quantity: 2,
+      unitPrice: 16,
+      totalPrice: 32,
+    },
+  ],
+};
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addItem(state, action) {
+      state.cart.push(action.payload);
+    },
+    deleteItem(state, action) {
+      state.cart = state.cart.filter((item) => item.pizzaId !== action.payload);
+    },
+    increaseItemQuantity(state, action) {
+      const item = state.cart.find((item) => item.pizzaId === action.payload);
+      item.quantity++;
+      item.totalPrice = item.quantity * item.unitPrice;
+    },
+    decreaseItemQuantity(state, action) {
+      const item = state.cart.find((item) => item.pizzaId === action.payload);
+      item.quantity--;
+      item.totalPrice = item.quantity * item.unitPrice;
+    },
+    clearCart(state) {
+      state.cart = [];
+    },
+  },
+});
+
+export default cartSlice.reducer;
+export const {
+  addItem,
+  deleteItem,
+  increaseItemQuantity,
+  decreaseItemQuantity,
+  clearCart,
+} = cartSlice.actions;
--- Pizza Project Vite/src/features/menu/MenuItem.jsx
+++ Pizza Project Vite/src/features/menu/MenuItem.jsx
@@ -1,8 +1,25 @@
 import { formatCurrency } from "../utils/helpers";
 import Button from "../ui/Button";
+import { useDispatch } from "react-redux";
+import { addItem } from "../cart/cartSlice";
 
 function MenuItem({ pizza }) {
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+  const dispatch = useDispatch();
+
+  function handleAddToCart() {
+    console.log(id);
+
+    const newPizza = {
+      name: name,
+      pizzaId: id,
+      quantity: 1,
+      unitPrice: unitPrice,
+      totalPrice: unitPrice * 1,
+    };
+
+    dispatch(addItem(newPizza));
+  }
 
   return (
     <li className="flex gap-4 py-2">
@@ -22,7 +39,11 @@ function MenuItem({ pizza }) {
               Sold out
             </p>
           )}
-          <Button type="small">Add to cart</Button>
+          {!soldOut && (
+            <Button type="small" onClick={handleAddToCart}>
+              Add to cart
+            </Button>
+          )}
         </div>
       </div>
     </li>
