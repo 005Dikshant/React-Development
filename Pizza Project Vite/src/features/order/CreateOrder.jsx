398b70e79e99e2dc91ca837ebe4d13f2c5492fa2
Dikshant Yadav <37838049+005Dikshant@users.noreply.github.com>
2024-12-04 00:48:44 -0500

    Using cart for new Orders
    Keytake aways:
    
    1. useSelector
    2. we can use usedispatch inside a custom function by accessing the store directly and then dispatching the action, but it can lead to reduction in optimization, so be careful while using

--- Pizza Project Vite/src/features/order/CreateOrder.jsx
+++ Pizza Project Vite/src/features/order/CreateOrder.jsx
@@ -1,8 +1,12 @@
 import { useState } from "react";
 import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
 import { createOrder } from "../services/apiRestaurant";
-import Button from "../ui/Button";
 import { useSelector } from "react-redux";
+import { clearCart, getTotalCartPrice, getCart } from "../cart/cartSlice";
+import { formatCurrency } from "../utils/helpers";
+import Button from "../ui/Button";
+import EmptyCart from "../cart/EmptyCart";
+import store from "../../store";
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -10,39 +14,21 @@ const isValidPhone = (str) =>
     str,
   );
 
-const fakeCart = [
-  {
-    pizzaId: 12,
-    name: "Mediterranean",
-    quantity: 2,
-    unitPrice: 16,
-    totalPrice: 32,
-  },
-  {
-    pizzaId: 6,
-    name: "Vegetale",
-    quantity: 1,
-    unitPrice: 13,
-    totalPrice: 13,
-  },
-  {
-    pizzaId: 11,
-    name: "Spinach and Mushroom",
-    quantity: 1,
-    unitPrice: 15,
-    totalPrice: 15,
-  },
-];
-
 function CreateOrder() {
-  // const [withPriority, setWithPriority] = useState(false);
-  const cart = fakeCart;
+  const [withPriority, setWithPriority] = useState(false);
+  const cart = useSelector(getCart);
+  const totalCartPrice = useSelector(getTotalCartPrice);
+  const priorityPrice = withPriority ? 0.2 * totalCartPrice : 0;
+
+  const totalPrice = totalCartPrice + priorityPrice;
 
   const navigation = useNavigation();
 
   const formErrors = useActionData();
 
   const username = useSelector((state) => state.user.username);
+
+  if (!cart.length) return <EmptyCart />;
 
   const isSubmitting = navigation.state === "submitting";
 
@@ -93,8 +79,8 @@ function CreateOrder() {
             name="priority"
             id="priority"
             className="h-6 w-6 accent-yellow-400 focus:outline-none focus:ring focus:ring-yellow-400 focus:ring-offset-2"
-            // value={withPriority}
-            // onChange={(e) => setWithPriority(e.target.checked)}
+            value={withPriority}
+            onChange={(e) => setWithPriority(e.target.checked)}
           />
           <label htmlFor="priority" className="font-medium">
             Want to yo give your order priority?
@@ -103,7 +89,9 @@ function CreateOrder() {
 
         <div>
           <Button type="primary" disabled={isSubmitting}>
-            {isSubmitting ? "Placing an order..." : "Order now"}
+            {isSubmitting
+              ? "Placing an order..."
+              : `Order now from ${formatCurrency(totalPrice)}`}
           </Button>
         </div>
         <div>
@@ -120,7 +108,7 @@ export async function action({ request }) {
 
   const order = {
     ...data,
-    priority: data.priority === "on" ? true : false,
+    priority: data.priority === "true" ? true : false,
     cart: JSON.parse(data.cart),
   };
 
@@ -132,6 +120,8 @@ export async function action({ request }) {
   if (Object.keys(errors).length > 0) return errors;
 
   const newOrder = await createOrder(order);
+
+  store.dispatch(clearCart()); // do not use overuse this hack
 
   return redirect(`/order/${newOrder.id}`);
 }
