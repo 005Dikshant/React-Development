86d2ba1476f4aa4c5447576de62cf53b443c49b3
Dikshant Yadav <37838049+005Dikshant@users.noreply.github.com>
2024-12-19 22:52:40 -0500

    useFetcher correction
    Keytake away:
    
    1. As per application requirement, we are using useFetcher inside different route ie order.jsx

--- Pizza Project Vite/src/features/menu/Menu.jsx
+++ Pizza Project Vite/src/features/menu/Menu.jsx
@@ -1,17 +1,10 @@
-import { useFetcher, useLoaderData } from "react-router-dom";
+import { useLoaderData } from "react-router-dom";
 import { getMenu } from "../services/apiRestaurant";
 
 import MenuItem from "./MenuItem";
-import { useEffect } from "react";
 
 function Menu() {
   const menu = useLoaderData();
-
-  const fetcher = useFetcher();
-
-  useEffect(() => {
-    fetcher.load("/menu");
-  }, [fetcher]);
 
   return (
     <ul className="divide-y divide-stone-200 px-2">
--- Pizza Project Vite/src/features/order/Order.jsx
+++ Pizza Project Vite/src/features/order/Order.jsx
@@ -1,12 +1,21 @@
 // Test ID: IIDSAT
 
-import { useLoaderData } from "react-router-dom";
+import { useFetcher, useLoaderData } from "react-router-dom";
 import { getOrder } from "../services/apiRestaurant";
 import { calcMinutesLeft, formatCurrency, formatDate } from "../utils/helpers";
 import OrderItem from "./OrderItem";
+import { useEffect } from "react";
 
 function Order() {
   const order = useLoaderData();
+
+  const fetcher = useFetcher();
+
+  useEffect(() => {
+    if (!fetcher.data && fetcher.state === "idle") fetcher.load("/menu");
+  }, [fetcher]);
+
+  console.log(fetcher.data);
 
   // Everyone can search for all orders, so for privacy reasons we're gonna gonna exclude names or address, these are only for the restaurant staff
   const {
@@ -49,7 +58,7 @@ function Order() {
       </div>
       <ul className="divide-y divide-stone-200 border-b border-t">
         {cart.map((item) => (
-          <OrderItem item={item} key={item.id} />
+          <OrderItem item={item} key={item.pizzaId} />
         ))}
       </ul>
 
